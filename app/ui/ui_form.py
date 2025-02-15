014bf1f0123e32723169e590bcaa7082733dc0ad
pfczx <marcel.ras@edu.ueakat.pl>
2025-02-15 18:44:14 +0100

    added ui

---
+++ app/ui/ui_form.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QPushButton,
+    QSizePolicy, QWidget)
+
+class Ui_Widget(object):
+    def setupUi(self, Widget):
+        if not Widget.objectName():
+            Widget.setObjectName(u"Widget")
+        Widget.resize(903, 600)
+        Widget.setContextMenuPolicy(Qt.ContextMenuPolicy.PreventContextMenu)
+        Widget.setAutoFillBackground(False)
+        Widget.setStyleSheet(u"background: qlineargradient(x1:0, y1:0, x2:1, y2:1,\n"
+"                                stop:0 #706dad, stop:1 #c9d6ff);")
+        self.pushButton = QPushButton(Widget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(20, 30, 361, 51))
+        self.pushButton.setStyleSheet(u"    background-color: #6c5ce7; /* Fioletowe t\u0142o */\n"
+"    color: white; /* Bia\u0142y tekst */\n"
+"    border: 2px solid #5a4fcf; /* Ciemnofioletowa ramka */\n"
+"    border-radius: 8px; /* Zaokr\u0105glone rogi */\n"
+"    padding: 10px; /* Wewn\u0119trzny odst\u0119p */\n"
+"    font-size: 14px; /* Rozmiar czcionki */\n"
+"    font-weight: bold; /* Pogrubiony tekst */")
+        self.pushButton_2 = QPushButton(Widget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(430, 30, 451, 51))
+        self.pushButton_2.setMaximumSize(QSize(451, 16777215))
+        self.pushButton_2.setStyleSheet(u"    background-color: #6c5ce7; /* Fioletowe t\u0142o */\n"
+"    color: white; /* Bia\u0142y tekst */\n"
+"    border: 2px solid #5a4fcf; /* Ciemnofioletowa ramka */\n"
+"    border-radius: 8px; /* Zaokr\u0105glone rogi */\n"
+"    padding: 10px; /* Wewn\u0119trzny odst\u0119p */\n"
+"    font-size: 14px; /* Rozmiar czcionki */\n"
+"    font-weight: bold; /* Pogrubiony tekst */")
+        self.pushButton_3 = QPushButton(Widget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QRect(160, 530, 131, 41))
+        self.pushButton_3.setStyleSheet(u"    background-color: #6c5ce7; /* Fioletowe t\u0142o */\n"
+"    color: white; /* Bia\u0142y tekst */\n"
+"    border: 2px solid #5a4fcf; /* Ciemnofioletowa ramka */\n"
+"    border-radius: 8px; /* Zaokr\u0105glone rogi */\n"
+"    padding: 10px; /* Wewn\u0119trzny odst\u0119p */\n"
+"    font-size: 14px; /* Rozmiar czcionki */\n"
+"    font-weight: bold; /* Pogrubiony tekst */")
+        self.pushButton_4 = QPushButton(Widget)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.pushButton_4.setGeometry(QRect(300, 530, 131, 41))
+        self.pushButton_4.setStyleSheet(u"    background-color: #6c5ce7; /* Fioletowe t\u0142o */\n"
+"    color: white; /* Bia\u0142y tekst */\n"
+"    border: 2px solid #5a4fcf; /* Ciemnofioletowa ramka */\n"
+"    border-radius: 8px; /* Zaokr\u0105glone rogi */\n"
+"    padding: 10px; /* Wewn\u0119trzny odst\u0119p */\n"
+"    font-size: 14px; /* Rozmiar czcionki */\n"
+"    font-weight: bold; /* Pogrubiony tekst */")
+        self.pushButton_5 = QPushButton(Widget)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+        self.pushButton_5.setGeometry(QRect(530, 530, 111, 41))
+        self.pushButton_5.setStyleSheet(u"    background-color: #6c5ce7; /* Fioletowe t\u0142o */\n"
+"    color: white; /* Bia\u0142y tekst */\n"
+"    border: 2px solid #5a4fcf; /* Ciemnofioletowa ramka */\n"
+"    border-radius: 8px; /* Zaokr\u0105glone rogi */\n"
+"    padding: 10px; /* Wewn\u0119trzny odst\u0119p */\n"
+"    font-size: 14px; /* Rozmiar czcionki */\n"
+"    font-weight: bold; /* Pogrubiony tekst */")
+        self.label = QLabel(Widget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(160, 160, 561, 271))
+        font = QFont()
+        font.setBold(True)
+        self.label.setFont(font)
+        self.label.setStyleSheet(u"    background-color: rgba(113, 113, 255, 0.8); /* P\u00f3\u0142przezroczyste bia\u0142e t\u0142o */\n"
+"    color: #2d3436; /* Ciemnoszary tekst */\n"
+"    border: 9px solid #7365b8; /* Jasnoszara ramka */\n"
+"    border-radius: 8px; /* Zaokr\u0105glone rogi */\n"
+"    padding: 15px; /* Wewn\u0119trzny odst\u0119p */\n"
+"    font-size: 33px; /* Rozmiar czcionki */\n"
+"    font-weight: bold; /* Pogrubiony tekst */\n"
+"    text-align: center; /* Wy\u015brodkowanie tekstu */")
+        self.label.setFrameShape(QFrame.Shape.Box)
+        self.label.setFrameShadow(QFrame.Shadow.Plain)
+        self.label.setLineWidth(6)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.retranslateUi(Widget)
+
+        QMetaObject.connectSlotsByName(Widget)
+    # setupUi
+
+    def retranslateUi(self, Widget):
+        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
+        self.pushButton.setText(QCoreApplication.translate("Widget", u"Wybierz zestaw", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Widget", u"dodaj zestaw", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Widget", u"poprzedni", None))
+        self.pushButton_4.setText(QCoreApplication.translate("Widget", u"nastepny", None))
+        self.pushButton_5.setText(QCoreApplication.translate("Widget", u"losowy", None))
+        self.label.setText(QCoreApplication.translate("Widget", u"Fiszka", None))
+    # retranslateUi
+
